bb9f11dc1007b724f439b07cea8439cf27dd9839
Ícaro Gabryel <icarogabryel2001@gmail.com>
2025-02-11 20:16:47 -0300

    bin added to parser, assign node refractured and parser tested. Test folder refractured

---
+++ flooat/ast.py
@@ -0,0 +1,139 @@
+class Mod:
+    def __init__(self) -> None:
+        self.comps = []
+
+    def add_comp(self, comp):
+        self.comps.append(comp)
+
+    def __repr__(self) -> str:
+        repr = ''
+
+        for comp in self.comps:
+            repr += f'{comp} '
+
+        return f'Mod({self.comps})'
+    
+    def __str__(self) -> str:
+        string = ''
+
+        for comp in self.comps:
+            string += f'{comp}\n\n'
+
+        return string
+
+
+class Comp:
+    def __init__(self) -> None:
+        self.id = ''
+        self.is_main = False
+        self.stmts = []
+
+    def add_stmt(self, stmt):
+        self.stmts.append(stmt)
+
+    def __repr__(self) -> str:
+        repr = ''
+
+        for stmt in self.stmts:
+            repr += f'{stmt} '
+
+        return f'Comp({self.id}, {self.is_main}, {self.stmts})'
+    
+    def __str__(self) -> str:
+        string = f'|-- Comp: {self.id}'
+
+        if self.is_main:
+            string += ' (main)'
+
+        string += '\n'
+
+        for stmt in self.stmts:
+            stmt_str = str(stmt).replace('\n', '\n|  ')
+            string += f'|  |-- {stmt_str}\n'
+
+        return string
+
+
+class Decl:
+    def __init__(self) -> None:
+        self.id = ''
+        self.conn = 0
+        self.type = 'bit'
+
+    def __repr__(self) -> str:
+        return f'Decl({self.id}, {self.type})'
+    
+    def __str__(self) -> str:
+        string = f'Decl: {self.id} ({self.type}'
+
+        if self.conn == -1:
+            string += ', input)'
+        elif self.conn == 1:
+            string += ', output)'
+        else:
+            string += ', internal)'
+
+        return string
+
+
+class Assign:
+    def __init__(self) -> None:
+        self.dt: str = None
+        self.expr = None
+
+    def __repr__(self) -> str:
+        return f'Assign({self.dt}, {self.expr})'
+    
+    def __str__(self) -> str:
+        expr_str = str(self.expr).replace('\n', '\n|    ')
+        return f'Assign\n|  |-- {self.dt}\n|-- {expr_str}'
+
+
+class UnaryOp:
+    def __init__(self, op: str, expr: str) -> None:
+        self.op = op
+        self.expr = expr
+
+    def __repr__(self) -> str:
+        return f'({self.op} {self.expr})'
+    
+    def __str__(self) -> str:
+        value_str = str(self.value).replace('\n', '\n|  ')
+        return f'{self.op}\n|-- {value_str}'
+
+
+class BinaryOp:
+    def __init__(self, l_expr: str, op: str, r_expr: str) -> None:
+        self.l_expr = l_expr
+        self.op = op
+        self.r_expr = r_expr
+
+    def __repr__(self) -> str:
+        return f'{self.op}\n|-- {self.l_expr}\n|-- {self.r_expr}'
+    
+    def __str__(self) -> str:
+        l_expr_str = str(self.l_expr).replace('\n', '\n|  ')
+        r_expr_str = str(self.r_expr).replace('\n', '\n|  ')
+        return f'{self.op}\n|-- {l_expr_str}\n|-- {r_expr_str}'
+
+
+class Identifier:
+    def __init__(self, id: str) -> None:
+        self.id = id
+
+    def __repr__(self) -> str:
+        return f'Identifier {self.id}'
+    
+    def __str__(self) -> str:
+        return self.__repr__()
+
+
+class Binary:
+    def __init__(self, value: str) -> None:
+        self.value = value
+
+    def __repr__(self) -> str:
+        return f'Binary {self.value}'
+    
+    def __str__(self) -> str:
+        return self.__repr__()
--- test/integration_test.py
+++ test/integration_test.py
@@ -4,11 +4,12 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from flooat import Scanner
+from flooat.scanner import Scanner
+from flooat.parser import Parser
 
 
 def main():
-    with open('example/incrementor.ft', 'r') as file:
+    with open('src/incrementor.ft', 'r') as file:
         code = file.read()
 
     scanner = Scanner(code)
@@ -16,6 +17,12 @@ def main():
     print("Token Stream:\n")
     for token in scanner.token_stream:  # Print the token stream
         print(token)
+
+    parser = Parser(scanner.token_stream)
+
+    print("\nAST:\n")
+    print(parser.ast)  # Print the AST
+
 
 if __name__ == "__main__":
     main()
