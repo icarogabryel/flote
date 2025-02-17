da48dbdf41a645547b664e6aea0a0fa7d35c7086
Ícaro Gabryel <icarogabryel2001@gmail.com>
2025-02-17 19:48:52 -0300

    refactor: replace Visitor with Builder in make_comp function

--- flooat/__init__.py
+++ flooat/__init__.py
@@ -1,15 +1,14 @@
 from .scanner import Scanner
 from .parser import Parser
-from .visitor import Visitor
+from .builder import Builder
 from .component import Component
 
 def make_comp(code):
     scanner = Scanner(code)
     parser = Parser(scanner)
     print(parser.ast)
-    visitor = Visitor(parser)
+    builder = Builder(parser)
 
-    component = visitor.get_component()
-    component.token_stream = scanner.token_stream
+    component = builder.get_component()
 
     return component
