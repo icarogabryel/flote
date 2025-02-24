d6bcb3af80944b784fd163e6a69cd6d032b9a57f
Ícaro Gabryel <icarogabryel2001@gmail.com>
2025-02-24 19:01:17 -0300

    refactor: replace dictionaries with list for keywords and comments and docstrings improved

--- flooat/scanner.py
+++ flooat/scanner.py
@@ -4,20 +4,20 @@ import re
 IGNORED_CHARS = [' ', '\t', '\n']
 END_OF_FILE = '\0'
 END_OF_COMMENT = ['\n', END_OF_FILE]
-KEY_WORDS_DICT = {
-    'main': 'main',
-    'comp': 'comp',
-    'in': 'in',
-    'out': 'out',
-    'bit': 'bit',
-    'not': 'not',
-    'nor': 'nor',
-    'and': 'and',
-    'nand': 'nand',
-    'xor': 'xor',
-    'or': 'or',
-}
-SYMBOLS_DICT = {
+KEY_WORDS = [
+    'main',
+    'comp',
+    'in',
+    'out',
+    'bit',
+    'not',
+    'nor',
+    'and',
+    'nand',
+    'xor',
+    'or',
+]
+SYMBOLS_LABELS = {
     ';': 'semicolon',
     '(': 'l_paren',
     ')': 'r_paren',
@@ -29,8 +29,8 @@ SYMBOLS_DICT = {
 
 class LexicalError(Exception):
     def __init__(self, line_number, message):
-        self.message = message
         self.line_number = line_number
+        self.message = message
 
     def __str__(self):
         return f"Lexical Error at line {self.line_number}: {self.message}"
@@ -61,28 +61,34 @@ class Scanner():
         self.index += 1
 
     def get_char(self) -> str:
+        """Return the current character pointed by self.index."""
+
         return self.code[self.index]
 
     def is_eof(self) -> bool:
         return True if self.get_char() == END_OF_FILE else False
 
     def skip_ignored(self):
+        """Skip ignored characters and comments."""
+
         while not self.is_eof():  # While don't reach the end of the string
             while self.get_char() in IGNORED_CHARS:  # Skip ignored characters
                 self.advance()
 
             if self.get_char() == '/':  # Skip line comments
-                if self.code[self.index + 1] == '/':  # Prevent self.index + 1 out of range
+                if self.code[self.index + 1] == '/':  # Prevent self.index + 1 out of range because of END_OF_FILE
                     while self.get_char() not in END_OF_COMMENT:
                         self.advance()
             else:  # If it's not an ignored character, break the loop
                 break
 
     def read(self):  # Read the next lexeme
+        """Form a lexeme by reading characters until a symbol or an ignored character is found."""
+
         lexeme = ''
 
         while not self.is_eof():  # While don't reach the end of the string
-            if (char := self.get_char()) in SYMBOLS_DICT: # If it's a symbol, stop reading and return the lexeme
+            if (char := self.get_char()) in SYMBOLS_LABELS: # If it's a symbol, stop reading and return the lexeme
                 return lexeme
             elif char in IGNORED_CHARS: # If it's an ignored character, stop reading and return the lexeme
                 return lexeme
@@ -94,20 +100,21 @@ class Scanner():
 
     def get_token(self):
         self.skip_ignored()
+
         token = None
 
         if self.is_eof():
             token = Token('EOF', END_OF_FILE)
 
-        elif (char := self.get_char()) in SYMBOLS_DICT:
-            token = Token(SYMBOLS_DICT[char], char)
+        elif (char := self.get_char()) in SYMBOLS_LABELS:
+            token = Token(SYMBOLS_LABELS[char], char)
             self.advance()
 
         elif re.match(r'[a-zA-Z_\d]', char):  # Check if the character can be the start of a word
             lexeme = self.read()
 
-            if lexeme in KEY_WORDS_DICT:
-                token = Token(KEY_WORDS_DICT[lexeme], lexeme)
+            if lexeme in KEY_WORDS:
+                token = Token(lexeme, lexeme)  # The label is the lexeme itself in case of keywords
 
             elif re.match(r'^[a-zA-Z]\w*$', lexeme):  # Check if the lexeme is a valid identifier
                 token = Token('id', lexeme)
@@ -120,6 +127,8 @@ class Scanner():
 
         else:
             raise LexicalError(self.line_number, f"Invalid character: {char}")
+
+        assert token is not None  #* Here I am another day. Under the bloodthirsty eye of the debugger
 
         return token
 
