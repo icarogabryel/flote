872c0e2e76309761299f37072dfb01f047beb560
Ícaro Gabryel <icarogabryel2001@gmail.com>
2025-06-14 15:18:34 -0300

    refactor: update file path handling and improve docstring formatting

--- test/integration_test.py
+++ test/integration_test.py
@@ -1,30 +1,30 @@
-# Tests for the integration of Scanner, Parser, and Builder classes in the Flote project.
-
-import os
-import sys
-
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+"""
+Tests for the integration of Scanner, Parser, and Builder classes in the Flote
+project.
+"""
+from pathlib import Path
 from flote.scanner import Scanner
 from flote.parser import Parser
 from flote.builder import Builder
 
+
+FILE_DIR = Path(__file__).resolve().parent
 # 1 for Scanner, 2 for Parser, 3 for Builder
 TEST_MODE = 0
 
 
 def main():
-    with open('src/halfAdder.ft', 'r') as file:
+    with open(FILE_DIR / 'src/halfAdder.ft') as file:
         code = file.read()
 
     if TEST_MODE > 0:
         scanner = Scanner(code)
-        
+
         print('Token Stream:\n')
 
         tokens = scanner.get_token_stream()
         for token in tokens:
             print(token)
-
 
     if TEST_MODE > 1:
         scanner = Scanner(code)
