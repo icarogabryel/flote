137026e41d414df7d920bf5aa3256c57a35c755e
Ícaro Gabryel <icarogabryel2001@gmail.com>
2025-06-14 19:05:08 -0300

    refactor: update import statements and file path for VCD output

--- test/test_integration_build.py
+++ test/test_integration_build.py
@@ -1,29 +1,33 @@
 """This file test de constructions of a RS Latch using the Flote library."""
-import flote as ft
+from flote.component import Component, BitBus
+from pathlib import Path
 
 
-latch = ft.Component('latch')
+FILE_DIR = Path(__file__).resolve().parent
 
-set = ft.BitBus()
-rst = ft.BitBus()
 
-q = ft.BitBus()
+latch = Component('latch')
+
+set = BitBus()
+rst = BitBus()
+
+q = BitBus()
 q.assignment = lambda: not (
     latch.bus_dict['rst'].value or latch.bus_dict['i1'].value
 )
 q.sensitivity_list = ['rst', 'i1']
 
-not_q = ft.BitBus()
+not_q = BitBus()
 not_q.assignment = lambda: not (
     latch.bus_dict['set'].value or latch.bus_dict['i2'].value
 )
 not_q.sensitivity_list = ['set', 'i2']
 
-i1 = ft.BitBus()
+i1 = BitBus()
 i1.assignment = lambda: latch.bus_dict['not_q'].value
 i1.sensitivity_list = ['not_q']
 
-i2 = ft.BitBus()
+i2 = BitBus()
 i2.assignment = lambda: latch.bus_dict['q'].value
 i2.sensitivity_list = ['q']
 
@@ -57,4 +61,4 @@ latch.wait(1)
 latch.stimulate({'set': '0', 'rst': '0'})
 latch.wait(1)
 
-latch.save_vcd('waves/latch_fw.vcd')
+latch.save_vcd(FILE_DIR / 'waves/RsLatch (fw).vcd')
