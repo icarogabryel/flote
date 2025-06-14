044161d951b0cfc8b3fe724c815a20d2377e22b8
Ícaro Gabryel <icarogabryel2001@gmail.com>
2025-06-14 19:02:13 -0300

    refactor: encapsulate half adder test logic in a function and update VCD file versions

--- test/test_system_half_adder_tb.py
+++ test/test_system_half_adder_tb.py
@@ -1,18 +1,27 @@
 import flote as ft
+from pathlib import Path
 
 
-half_adder = ft.elaborate_from_file('src/halfAdder.ft')
+FILE_DIR = Path(__file__).resolve().parent
 
-half_adder.stimulate({'a': '0', 'b': '0'})
-half_adder.wait(10)
 
-half_adder.stimulate({'a': '0', 'b': '1'})
-half_adder.wait(10)
+def test():
+    half_adder = ft.elaborate_from_file(FILE_DIR / 'src/HalfAdder.ft')
 
-half_adder.stimulate({'a': '1', 'b': '0'})
-half_adder.wait(10)
+    half_adder.stimulate({'a': '0', 'b': '0'})
+    half_adder.wait(10)
 
-half_adder.stimulate({'a': '1', 'b': '1'})
-half_adder.wait(10)
+    half_adder.stimulate({'a': '0', 'b': '1'})
+    half_adder.wait(10)
 
-half_adder.save_vcd('halfAdder.vcd')
+    half_adder.stimulate({'a': '1', 'b': '0'})
+    half_adder.wait(10)
+
+    half_adder.stimulate({'a': '1', 'b': '1'})
+    half_adder.wait(10)
+
+    half_adder.save_vcd(FILE_DIR / 'waves/HalfAdder.vcd')
+
+
+if __name__ == '__main__':
+    test()
