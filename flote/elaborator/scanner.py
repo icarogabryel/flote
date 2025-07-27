25198dc2d3fb9a7cfaac355ddc4f2274de29a074
Ícaro Gabryel <icarogabryel2001@gmail.com>
2025-07-27 15:37:47 -0300

    refactor: include line number in Token initialization and string representation

--- flote/elaborator/scanner.py
+++ flote/elaborator/scanner.py
@@ -43,12 +43,13 @@ class LexicalError(Exception):
 
 class Token():
     """Token class represents a lexical token with a label and a lexeme."""
-    def __init__(self, label, lexeme):
-        self.label = label
-        self.lexeme = lexeme
+    def __init__(self, line_number, label, lexeme):
+        self.line_number: int = line_number
+        self.label: str = label
+        self.lexeme: str = lexeme
 
     def __str__(self):
-        return f'({self.label}, "{self.lexeme}")'
+        return f'({self.line_number}. {self.label} - "{self.lexeme}")'
 
     def __repr__(self):
         return self.__str__()
@@ -135,10 +136,10 @@ class Scanner():
         token = None
 
         if self.is_eof():  # First, check if we reached the end of the code.
-            token = Token('EOF', END_OF_FILE)
+            token = Token(self.line_number, 'EOF', END_OF_FILE)
         # Check if the current character is a symbol.
         elif (char := self.get_char()) in SYMBOLS_LABELS:
-            token = Token(SYMBOLS_LABELS[char], char)
+            token = Token(self.line_number, SYMBOLS_LABELS[char], char)
             self.advance()
         # Check if the character can be the start of a word.
         elif re.match(r'[a-zA-Z_\d\"]', char):
@@ -146,18 +147,18 @@ class Scanner():
 
             if lexeme in KEY_WORDS:
                 # The label is the lexeme itself in case of keywords
-                token = Token(lexeme, lexeme)
+                token = Token(self.line_number, lexeme, lexeme)
             # Check if the lexeme is a valid identifier
             elif re.match(r'^[a-zA-Z]\w*$', lexeme):
-                token = Token('id', lexeme)
+                token = Token(self.line_number, 'id', lexeme)
             # Check if the lexeme is a valid binary number
             elif re.match(r'^\"[0-1]+\"$', lexeme):
-                token = Token('bit_field', lexeme)
+                token = Token(self.line_number, 'bit_field', lexeme)
             # Check if the lexeme is decimal number
             elif re.match(r'^[0-9]+$', lexeme):
                 # Check if the lexeme is a valid decimal number
                 if re.match(r'^[1-9][0-9]*$', lexeme) or lexeme == '0':
-                    token = Token('dec', lexeme)
+                    token = Token(self.line_number, 'dec', lexeme)
                 # If the lexeme is not a valid decimal number, raise an error.
                 else:
                     raise LexicalError(
