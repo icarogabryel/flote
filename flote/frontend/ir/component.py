92f20e84a6f36107d6442ae11e6ea2996a9561ef
Ícaro Gabryel <icarogabryel2001@gmail.com>
2025-09-23 19:33:05 -0300

    refactor: organiza pasta do core temp e readiciona estrutura para ir

---
+++ flote/frontend/ir/component.py
@@ -0,0 +1,48 @@
+from .busses import Bus
+
+
+class Component:
+    def __init__(self) -> None:
+        self.busses: dict[str, Bus] = {}
+
+    def __repr__(self):
+        return f'{'\n'.join([bus.__str__() for bus in self.busses.values()])}'
+
+    def __str__(self) -> str:
+        return self.__repr__()
+
+    def add_bus(self, bus_id: str, bus: Bus) -> None:
+        """
+        This method adds a bus to the component.
+
+        Args:
+            bus (Bus): The bus to be added.
+        """
+        assert bus_id not in self.busses, f'Bus with id "{bus_id}" already exists.'
+
+        self.busses[bus_id] = bus
+
+    def add_busses(self, component_id, buses: dict[str, Bus]) -> None:
+        """
+        This method adds multiple buses to the component.
+
+        Args:
+            buses (dict[str, Bus]): The buses to be added.
+        """
+        for bus_id, bus in buses.items():
+            key = f'{component_id}.{bus_id}'
+            self.busses[key] = bus
+
+    def add_component(self, component_id: str, component: 'Component') -> None:
+        """
+        This method adds the busses of a component to the current component.
+
+        Args:
+            component (Component): The component to be added.
+        """
+        for bus_id, bus in component.busses.items():
+            new_id = f'{component_id}.{bus_id}'
+
+            assert new_id not in self.busses, f'Bus with id "{new_id}" already exists.'
+
+            self.busses[new_id] = bus
---
+++ flote/frontend/ir/expr_nodes.py
@@ -0,0 +1,124 @@
+from .busses import BusValue, Evaluator
+from .component import Component
+from abc import ABC, abstractmethod
+
+
+class ExprNode():
+    def get_sensitivity_list(self):
+        return []
+
+
+class BusRef(Evaluator, ExprNode):
+    """This class represents a reference to a bus in the circuit."""
+    def __init__(self, bus):
+        self.bus = bus
+
+    def __repr__(self) -> str:
+        return f'{self.bus.id}'
+
+    def evaluate(self) -> BusValue:
+        return self.bus.value
+
+    def get_sensitivity_list(self):
+        return [self.bus]
+
+
+
+class Const(Evaluator):
+    def __init__(self, value: BusValue) -> None:
+        self.value = value
+
+    def __repr__(self) -> str:
+        return f'Const({self.value})'
+
+    def evaluate(self) -> BusValue:
+        return self.value
+
+
+#TODO type all this
+class UnaryOperation(Evaluator):
+    def __init__(self, expr: Evaluator) -> None:
+        self.expr = expr
+
+    def __repr__(self) -> str:
+        return self.__str__()
+
+    def __str__(self) -> str:
+        return f'{self.__class__.__name__} ({self.expr})'
+
+    def get_sensitivity_list(self):
+        return self.expr.get_sensitivity_list()
+
+
+class BinaryOperation(Evaluator):
+    def __init__(self, l_expr: Evaluator, r_expr: Evaluator) -> None:
+        self.l_expr = l_expr
+        self.r_expr = r_expr
+
+    def __repr__(self) -> str:
+        return self.__str__()
+
+    def __str__(self) -> str:
+        return f'({self.l_expr}) {self.__class__.__name__} ({self.r_expr})'
+
+    def get_sensitivity_list(self):
+        return self.l_expr.get_sensitivity_list() + self.r_expr.get_sensitivity_list()
+
+
+class Not(UnaryOperation):
+    def __repr__(self) -> str:
+        return f'Not {self.expr}'
+
+    def evaluate(self):
+        return ~ self.expr.evaluate()
+
+
+class And(BinaryOperation):
+    def __repr__(self) -> str:
+        return f'And {self.l_expr} {self.r_expr}'
+
+    def evaluate(self):
+        return self.l_expr.evaluate() & self.r_expr.evaluate()
+
+
+class Or(BinaryOperation):
+    def __repr__(self) -> str:
+        return f'Or {self.l_expr} {self.r_expr}'
+
+    def evaluate(self):
+        return self.l_expr.evaluate() | self.r_expr.evaluate()
+
+
+class Xor(BinaryOperation):
+    def __repr__(self) -> str:
+        return f'Xor {self.l_expr} {self.r_expr}'
+
+    def evaluate(self):
+        return self.l_expr.evaluate() ^ self.r_expr.evaluate()
+
+
+class Nand(BinaryOperation):
+    def __repr__(self) -> str:
+        return f'Nand {self.l_expr} {self.r_expr}'
+
+    def evaluate(self):
+        return ~ (self.l_expr.evaluate() & self.r_expr.evaluate())
+
+
+class Nor(BinaryOperation):
+    def __repr__(self) -> str:
+        return f'Nor {self.l_expr} {self.r_expr}'
+
+    def evaluate(self):
+        return ~ (self.l_expr.evaluate() | self.r_expr.evaluate())
+
+
+class Xnor(BinaryOperation):
+    def __repr__(self) -> str:
+        return f'Xnor {self.l_expr} {self.r_expr}'
+
+    def evaluate(self):
+        return ~ (self.l_expr.evaluate() ^ self.r_expr.evaluate())
+
+
+Operations = And | Or | Xor | Nand | Nor | Xnor | Not
