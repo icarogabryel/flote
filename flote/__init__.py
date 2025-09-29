8cb0c161f161faadbc9d004447aac9a17b19238d
Ícaro Gabryel <icarogabryel2001@gmail.com>
2025-09-29 20:50:46 -0300

    refactor: reorganize Renderer import and clean up unused code

--- flote/__init__.py
+++ flote/__init__.py
@@ -1,16 +1,19 @@
-from warnings import warn
+# from warnings import warn
 
 from .frontend.builder import Builder
 from .frontend.parser import Parser
 from .frontend.scanner import Scanner
 from .test_bench import TestBench
 
-try:
-    from .backend.rust.core import Renderer
-except ImportError:
-    warn('Rust backend not available, using Python backend.')
+#TODO implement Rust backend
+# try:
+#     from .backend.rust.core import Renderer
+# except ImportError:
+#     warn('Rust backend not available, using Python backend.')
 
-    from .backend.python.core.renderer import Renderer
+#     from .backend.python.core import Renderer
+
+from .backend.python.core import Renderer
 
 
 def elaborate(code: str) -> TestBench:
@@ -36,3 +39,16 @@ def elaborate_from_file(file_path):
         code = file.read()
 
     return elaborate(code)
+
+
+def comp_ir(code: str) -> str:
+    scanner = Scanner(code)
+    tokens_stream = scanner.token_stream
+
+    parser = Parser(tokens_stream)
+    ast = parser.ast
+
+    builder = Builder(ast)
+    ir = builder.ir
+
+    return ir
