70538360a0e3d0d5ec1561bd4de6b6b5b785a836
Ícaro Gabryel <icarogabryel2001@gmail.com>
2025-10-11 21:46:29 -0300

    tests: update make_ir function to accept source_code and streamline IR generation

--- tests/exploration/frontend.py
+++ tests/exploration/frontend.py
@@ -1,44 +1,59 @@
-from json import dumps
+import sys
+from pathlib import Path
 
 from flote.frontend.builder import Builder
 from flote.frontend.parser import Parser
 from flote.frontend.scanner import Scanner
 
-from tests.utils import BASE_DIR
 
-
+BASE_DIR = Path(__file__).parent.parent.parent
 TESTS_DIR = BASE_DIR / 'tests'
 
 
-def make_ir(dut: str) -> str:
+def make_ir(source_code: str) -> str:
     """Helper function to create an IR from source code."""
 
-    scanner = Scanner(dut)
+    scanner = Scanner(source_code)
     token_stream = scanner.token_stream
-
-    assert token_stream is not None
 
     parser = Parser(token_stream)
     ast = parser.ast
 
     builder = Builder(ast)
     ir = builder.ir
-    ir.make_influence_lists()
 
-    ir_json = dumps(ir.to_json(), indent=4)
-
-    return ir_json
-
-
-def test_ir():
-    """Test the IR generation from source code."""
-    with open(TESTS_DIR / 'duts' / 'Inverter.ft', 'r') as f:
-        dut = f.read()
-
-    ir_json = make_ir(dut)
-
-    print(ir_json)
+    return ir
 
 
 if __name__ == '__main__':
-    test_ir()
+    """Test the IR generation from source code."""
+    duts_dir = TESTS_DIR / 'duts'
+    files = sorted([p for p in duts_dir.iterdir() if p.is_file()])
+
+    print("Available DUTs:")
+    for i, p in enumerate(files, 1):
+        print(f"{i}: {p.name}")
+
+    while True:
+        while True:
+            try:
+                choice = input("Choice the DUT number to see (or 'q' to exit): ").strip()
+                if choice.lower() == 'q':
+                    sys.exit(0)
+
+                idx = int(choice)
+                if 1 <= idx <= len(files):
+                    selected = files[idx - 1]
+                    break
+            except (ValueError, KeyboardInterrupt):
+                pass
+
+            print("Entrada inválida. Tente novamente.")
+
+        with open(selected, 'r', encoding='utf-8') as f:
+            dut = f.read()
+
+        print('\n')
+        ir_json = make_ir(dut)
+        print(ir_json)
+        print('\n')
