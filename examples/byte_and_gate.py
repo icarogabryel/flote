ca95a0074560285d882c443f1940985261a16339
Ícaro Gabryel <icarogabryel2001@gmail.com>
2026-03-22 15:32:28 -0300

    test: Update examples

--- examples/byte_and_gate.py
+++ examples/byte_and_gate.py
@@ -1,15 +1,11 @@
 from pathlib import Path
 
-from flote import elaborate_from_file
+from flote import elaborate_file
 
-BASE_DIR = Path(__file__).parent.parent.parent
-TESTS_DIR = BASE_DIR / 'tests'
-
-byte_and_gate = elaborate_from_file(
-    TESTS_DIR / 'duts' / 'ByteAndGate.ft'
+BASE_DIR = Path(__file__).parent.parent
+byte_and_gate = elaborate_file(
+    BASE_DIR / 'examples' / 'ByteAndGate.ft'
 )
-
-print(byte_and_gate)
 
 byte_and_gate.update({'a': '00000000', 'b': '00000000'})
 byte_and_gate.wait(10)
@@ -19,7 +15,8 @@ byte_and_gate.wait(10)
 
 byte_and_gate.update({'a': '11111111', 'b': '11111111'})
 byte_and_gate.wait(10)
-byte_and_gate.update({'a': '10101010', 'b': '11001100'})
+
+byte_and_gate.update({'a': '10101010', 'b': '11110000'})
 byte_and_gate.wait(10)
 
 byte_and_gate.save_vcd('ByteAndGate.vcd')
--- examples/full_adder.py
+++ examples/full_adder.py
@@ -1,49 +1,39 @@
 from pathlib import Path
 
-import flote as ft
+from flote import elaborate_file
 
-BASE_DIR = Path(__file__).parent.parent.parent
-TESTS_DIR = BASE_DIR / 'tests'
+BASE_DIR = Path(__file__).parent.parent
 
 
 def test():
-    full_adder = ft.elaborate_file(
-        TESTS_DIR / 'duts' / 'FullAdder.ft',
+    full_adder = elaborate_file(
+        BASE_DIR / 'examples' / 'FullAdder.ft',
     )
-    print(full_adder)
 
     # Test all 8 combinations of 3 input bits
     full_adder.update({'a': '0', 'b': '0', 'cin': '0'})
     full_adder.wait(10)
-    print(full_adder)
 
     full_adder.update({'a': '0', 'b': '0', 'cin': '1'})
     full_adder.wait(10)
-    print(full_adder)
 
     full_adder.update({'a': '0', 'b': '1', 'cin': '0'})
     full_adder.wait(10)
-    print(full_adder)
 
     full_adder.update({'a': '0', 'b': '1', 'cin': '1'})
     full_adder.wait(10)
-    print(full_adder)
 
     full_adder.update({'a': '1', 'b': '0', 'cin': '0'})
     full_adder.wait(10)
-    print(full_adder)
 
     full_adder.update({'a': '1', 'b': '0', 'cin': '1'})
     full_adder.wait(10)
-    print(full_adder)
 
     full_adder.update({'a': '1', 'b': '1', 'cin': '0'})
     full_adder.wait(10)
-    print(full_adder)
 
     full_adder.update({'a': '1', 'b': '1', 'cin': '1'})
     full_adder.wait(10)
-    print(full_adder)
 
     full_adder.save_vcd('FullAdder.vcd')
 
